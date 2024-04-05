ca1a3c1b9473bb1eade49e30b3a2aefc5d9ef403
sandra <sandra@iu.edu>
2024-04-05 02:56:49 -0400

    rating endpoint

--- src/controller/reservationController.js
+++ src/controller/reservationController.js
@@ -6,6 +6,7 @@ const {getProductByUserId} = require("../services/getProductByUserId");
 const getRentedProductByUserId = require("../services/getRentedProductByUserId");
 const {setProductStatus} = require("../services/setProductStatus");
 const {setTransactionStatus} = require("../services/setTransactionStatus");
+const {rateProduct} = require("../services/rateProduct");
 const reserveProduct = async(req,res) => {
     const request = req.body;
     if(!request){res.status(400).send({code:400,message:INVALID_REQUEST})}
@@ -64,4 +65,20 @@ const returnProductController = async(req,res) => {
 
 }
 
-module.exports = {reserveProduct,rentedProductByUserIdController,returnProductController}
+const rateProductController = async(req,res) => {
+    const request = req.body;
+    if(!request){res.status(400).send({code:400,message:INVALID_REQUEST})}
+    try{
+        const responseData = await rateProduct(request.productId,request.rating);
+        res.status(responseData.code).send(responseData);
+    }
+    catch (e) {
+        console.log(e);
+        res.status(500).send(
+            {code:500,message:UNKNOWN_ERROR}
+        );
+    }
+
+}
+
+module.exports = {reserveProduct,rentedProductByUserIdController,returnProductController,rateProductController}
--- src/routes/reservationRoutes.js
+++ src/routes/reservationRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const {reserveProduct, rentedProductByUserIdController, returnProductController} = require("../controller/reservationController");
+const {reserveProduct, rentedProductByUserIdController, returnProductController, rateProductController} = require("../controller/reservationController");
 const {productByUserIdController} = require("../controller/productsController");
 
 const router = express.Router();
@@ -9,5 +9,7 @@ router.post('/reserve',reserveProduct)
 router.get('/getRentedProductsByUserId',rentedProductByUserIdController);
 
 router.get('/returnProduct',returnProductController)
+
+router.post('/rateProduct',rateProductController)
 
 module.exports = router;
