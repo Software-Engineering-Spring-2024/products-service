0c2973f5286810e72db1eac14dbe189a7cae264d
sandra <sandra@iu.edu>
2024-04-18 21:26:56 -0400

    Ticket services

--- src/services/sendMail.js
+++ src/services/sendMail.js
@@ -4,6 +4,7 @@ const nodemailer = require('nodemailer');
 const {send} = require("express/lib/response");
 const {getUserDetailsById} = require("./getUserDetailsById");
 const {getProductById} = require("./getProductById");
+const getTicketById = require("./getTicketById");
 
 let transporter = nodemailer.createTransport({
     service: 'gmail',
@@ -61,6 +62,43 @@ const sendBookingMail = async (product_id, user_id) => {
     }
 };
 
+const sendTicketNotificationToOwner = async (ticket_id, owner_id) => {
+    let owner, ticket;
+
+    try {
+        // Fetch owner details
+        owner = await getUserDetailsById(owner_id);
+        if (!owner) {
+            console.error('Owner not found');
+            return false;
+        }
+        ownerEmail = owner[0].email;
+
+        // Fetch ticket details
+        ticket = await getTicketById(ticket_id);
+        console.log(ticket);
+        if (!ticket) {
+            console.error('Ticket not found');
+            return false;
+        }
+
+    } catch (error) {
+        console.error('Error fetching details:', error);
+        return false;
+    }
+
+    // Construct the email message based on the ticket details
+    const subject = `Ticket Update: ${ticket.title}`;
+    const message = `Hello ${owner[0].username},\nThere is a complaint regarding your product titled "${ticket.products.title}":\n${ticket.description}\n\nPlease review it at your earliest convenience.\n\nBest regards,\nYour Support Team`;
+
+    // Prepare mail context
+    const mailContext = setMailContext(ownerEmail, subject, message);
+
+    // Send the email
+    return sendMail(mailContext);
+};
+
+
 async function sendMail(mailOptions) {
     return new Promise((resolve, reject) => {
         transporter.sendMail(mailOptions, (error, info) => {
@@ -75,4 +113,4 @@ async function sendMail(mailOptions) {
     });
 }
 
-module.exports = sendBookingMail;
+module.exports = {sendBookingMail,sendTicketNotificationToOwner};
